6500cd6ee81902e9a2433c272bb2c33fa0f70b25
satishthulva <sathya691@gmail.com>
2017-10-31 19:44:23 +0530

    removed agreement

--- hbdr/src/main/java/com/novice/hbdr/FriendGroupParser.java
+++ hbdr/src/main/java/com/novice/hbdr/FriendGroupParser.java
@@ -1,21 +1,5 @@
 /*
  * FriendGroupParser.java
- *
- * Product: SCG
- * Strand Clinical Genomics
- *
- * Copyright 2007-2017, Strand Life Sciences
- * 5th Floor, Kirloskar Business Park, 
- * Bellary Road, Hebbal,
- * Bangalore 560024
- * India
- * All rights reserved.
- *
- * This software is the confidential and proprietary information
- * of Strand Life Sciences., ("Confidential Information").  You
- * shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with Strand Life Sciences.
  */
 package com.novice.hbdr;
 
--- hbdr/src/main/java/com/novice/hbdr/web/EndPoint.java
+++ hbdr/src/main/java/com/novice/hbdr/web/EndPoint.java
@@ -1,21 +1,5 @@
 /*
  * EndPoint.java
- *
- * Product: SCG
- * Strand Clinical Genomics
- *
- * Copyright 2007-2017, Strand Life Sciences
- * 5th Floor, Kirloskar Business Park, 
- * Bellary Road, Hebbal,
- * Bangalore 560024
- * India
- * All rights reserved.
- *
- * This software is the confidential and proprietary information
- * of Strand Life Sciences., ("Confidential Information").  You
- * shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with Strand Life Sciences.
  */
 package com.novice.hbdr.web;
 
@@ -77,7 +61,7 @@ public class EndPoint
     @Path("/newGroup")
     @POST
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public void registerNewGroup(/*FormDataMultiPart multipart, */@FormDataParam("groupFile") InputStream groupFile)
+    public void registerNewGroup(@FormDataParam("groupFile") InputStream groupFile)
     {
         try
         {
